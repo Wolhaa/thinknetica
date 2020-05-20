212fa10d61fae9fbce0e3b9f8a0dbae8a1cf55cd
Wolhaa <wolhaa@yahoo.com>
2020-05-20 07:05:51 +0300

    Fix_1

--- lesson_1/ideal_weight.rb
+++ lesson_1/ideal_weight.rb
@@ -2,7 +2,7 @@
  name = gets.chomp.capitalize
  puts "What is your height in centimeters?"
  height = gets.to_i
- ideal_weight = (height - 110)*1.15
+ ideal_weight = (height - 110) * 1.15
  if ideal_weight < 0
    puts "#{name}, your weight is already optimal!"
  else
